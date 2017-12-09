75b061ffecc6ccfbd69498f3142abb26c03e7ba4
Hanna Bennett <bennetthanna22@gmail.com>
2017-12-08 18:37:56 -0800

    Implement DELETE method

--- ToDoAPI/Controllers/ToDoController.cs
+++ ToDoAPI/Controllers/ToDoController.cs
@@ -73,5 +73,19 @@ namespace ToDoAPI.Controllers
             _context.SaveChanges();
             return new NoContentResult();
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todo = _context.ToDoItems.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.ToDoItems.Remove(todo);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
     }
 }
